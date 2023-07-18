f83ecebf27e77974b19613416268d6dd14aaae1c
citadin <citadinrodrigo@gmail.com>
2023-07-18 14:58:57 -0300

    add prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
