995599b42513355830b5f72dddbd6075ca12264d
citadin <citadinrodrigo@gmail.com>
2023-07-19 15:47:49 -0300

    add nest generate resource account

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { AccountModule } from './account/account.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PrismaModule],
+  imports: [ConfigModule.forRoot(), PrismaModule, AccountModule],
   controllers: [AppController],
   providers: [AppService],
 })
