0750bd4a63a026b9697ca21e812b77c69b8c37f8
citadin <citadinrodrigo@gmail.com>
2023-07-19 17:34:02 -0300

    add create account dto

--- src/account/dto/create-account.dto.ts
+++ src/account/dto/create-account.dto.ts
@@ -1,2 +1,59 @@
+import { IsBoolean, IsDecimal, IsEmail, IsEnum, IsInt, IsNotEmpty, IsOptional, IsString, IsStrongPassword } from "class-validator";
+import { Prisma } from '@prisma/client';
+
+enum Elo {
+  UNRANKED,
+  IRON,
+  SILVER,
+  GOLD,
+  PLATINUM,
+  ESMERALD,
+  DIAMOND,
+  MASTER,
+  GRANDMASTER,
+  CHALLENGER,
+}
+
 export class CreateAccountDto {
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsStrongPassword()
+  @IsNotEmpty()
+  password: string;
+
+  @IsBoolean()
+  @IsOptional()
+  sold: boolean
+
+  @IsInt()
+  @IsOptional()
+  level: number;
+
+  @IsEnum(Elo)
+  @IsOptional()
+  elo: Elo
+
+  @IsInt()
+  @IsNotEmpty()
+  blueEssence: number;
+
+  @IsInt()
+  @IsNotEmpty()
+  skinQuantity: number;
+
+  @IsInt()
+  @IsNotEmpty()
+  championQuantity: number;
+
+  @IsDecimal()
+  @IsNotEmpty()
+  price: Prisma.Decimal;
 }
