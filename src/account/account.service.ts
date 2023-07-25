bbf8ff4819eaabb872084393f4101df2e92edabe
citadin <citadinrodrigo@gmail.com>
2023-07-25 18:08:52 -0300

    create user service

--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -1,15 +1,19 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateAccountDto } from './dto/create-account.dto';
 import { UpdateAccountDto } from './dto/update-account.dto';
 import { PrismaService } from 'src/prisma.service';
-
+import { Account } from '@prisma/client';
 
 @Injectable()
 export class AccountService {
   constructor(private prisma: PrismaService) { }
 
-  create(createAccountDto: CreateAccountDto) {
-    // todo
+  async create(createAccountDto: CreateAccountDto): Promise<Account> {
+    const account: Account = await this.prisma.account.create({ data: createAccountDto })
+
+    if (!account) throw new BadRequestException;
+
+    return account;
   }
 
   findAll() {
