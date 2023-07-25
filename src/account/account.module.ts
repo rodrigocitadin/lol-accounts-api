d1435f6f3133bb81ab22ef0a4ed63943236c655a
citadin <citadinrodrigo@gmail.com>
2023-07-25 15:17:16 -0300

    add prisma to account module

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AccountService } from './account.service';
 import { AccountController } from './account.controller';
+import { PrismaModule } from 'src/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [AccountController],
   providers: [AccountService]
 })
-export class AccountModule {}
+export class AccountModule { }
