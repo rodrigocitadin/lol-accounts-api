0c8a9af1b12ea67aa03c6bf42307fecaa65c2562
citadin <citadinrodrigo@gmail.com>
2023-07-31 14:25:54 -0300

    create user dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsString, IsStrongPassword } from "class-validator";
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsStrongPassword()
+  @IsNotEmpty()
+  password: string;
+}
