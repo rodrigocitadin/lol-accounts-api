afceb759d342b4e4c32c8797893a5017dc9a4a7d
citadin <citadinrodrigo@gmail.com>
2023-07-31 13:22:50 -0300

    generate nest resource to user

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma.module';
 import { AccountModule } from './account/account.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PrismaModule, AccountModule],
+  imports: [ConfigModule.forRoot(), PrismaModule, AccountModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
