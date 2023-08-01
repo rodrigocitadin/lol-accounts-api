9d605448b6137b48d949b151ecdef98af1de39ba
citadin <citadinrodrigo@gmail.com>
2023-08-01 15:21:41 -0300

    delete an user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -32,7 +32,7 @@ export class UserController {
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+  async remove(@Param('id') id: string) {
+    await this.userService.remove(id);
   }
 }
