d4903ec11f56d8ed735dcd3f1a3007b40a1d3c5b
citadin <citadinrodrigo@gmail.com>
2023-08-07 09:36:52 -0300

    create auth resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma.module';
 import { AccountModule } from './account/account.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PrismaModule, AccountModule, UserModule],
+  imports: [ConfigModule.forRoot(), PrismaModule, AccountModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
