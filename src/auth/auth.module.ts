11e9a2cf30568701d4d4c1934e661d7d93fcdda7
citadin <citadinrodrigo@gmail.com>
2023-08-07 11:18:56 -0300

    config jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,22 @@ import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
 import { LocalStrategy } from './strategy/local.strategy';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+const configService: ConfigService = new ConfigService;
 
 @Module({
   controllers: [AuthController],
-  imports: [UserModule, PassportModule],
+  imports: [
+    UserModule,
+    PassportModule,
+    ConfigModule,
+    JwtModule.register({
+      secret: configService.get<string>('JWT_SECRET'),
+      signOptions: { expiresIn: '1 day' }
+    })
+  ],
   providers: [AuthService, LocalStrategy],
 })
 export class AuthModule { }
