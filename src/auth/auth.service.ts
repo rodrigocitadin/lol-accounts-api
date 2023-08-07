41b5f2d8344e6d6ed0d4f326947e636b1680ddf4
citadin <citadinrodrigo@gmail.com>
2023-08-07 11:12:15 -0300

    login jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
 import { AuthUser } from './types/auth-user';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) { }
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) { }
 
   async validateUser(username: string, password: string): Promise<AuthUser> {
     const user = await this.userService.findOne(username);
@@ -20,5 +24,8 @@ export class AuthService {
 
   async login(user: AuthUser) {
     const payload = { username: user.username, sub: user.id }
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
