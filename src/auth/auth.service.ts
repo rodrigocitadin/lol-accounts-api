e9318a47604f023627de02107828ac363c4184fc
citadin <citadinrodrigo@gmail.com>
2023-08-07 10:11:48 -0300

    create validate user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
-  }
+  constructor(private userService: UserService) { }
 
-  findAll() {
-    return `This action returns all auth`;
-  }
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.userService.findOne(username);
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
+    if (user && await bcrypt.compare(password, user.password)) {
+      const { password, ...result } = user;
+      return result
+    }
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+    return null;
   }
 }
