aba2f0e2af067e8d358898b0e779db982e65c1ab
citadin <citadinrodrigo@gmail.com>
2023-08-07 10:23:53 -0300

    fix some paths

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
-import { LocalStrategy } from './local.strategy';
+import { LocalStrategy } from './strategy/local.strategy';
 import { PassportModule } from '@nestjs/passport';
 
 @Module({
