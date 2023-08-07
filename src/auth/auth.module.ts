eb1ddb3bbd9dba3cc9c0c53012d7c93f3708ebf7
citadin <citadinrodrigo@gmail.com>
2023-08-07 10:11:27 -0300

    import user module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService]
+  imports: [UserModule],
+  providers: [AuthService],
 })
-export class AuthModule {}
+export class AuthModule { }
