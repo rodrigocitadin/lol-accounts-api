8ef153c952822ee565fc142463fd36edaf6c5785
citadin <citadinrodrigo@gmail.com>
2023-08-08 23:11:58 -0300

    add api property

--- src/account/dto/create-account.dto.ts
+++ src/account/dto/create-account.dto.ts
@@ -1,57 +1,71 @@
 import { IsBoolean, IsDecimal, IsEmail, IsEnum, IsInt, IsNotEmpty, IsOptional, IsString, IsStrongPassword } from "class-validator";
 import { Region, Elo, Prisma } from '@prisma/client';
+import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateAccountDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   username: string;
 
+  @ApiProperty()
   @IsString()
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({ required: false })
   @IsBoolean()
   @IsOptional()
   verifiedEmail: boolean;
 
+  @ApiProperty()
   @IsString()
   @IsStrongPassword()
   @IsNotEmpty()
   password: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   ownerId: string;
 
+  @ApiProperty({ required: false, default: false })
   @IsBoolean()
   @IsOptional()
   sold?: boolean;
 
+  @ApiProperty()
   @IsEnum(Region)
   @IsNotEmpty()
   region: Region;
 
+  @ApiProperty({ required: false })
   @IsInt()
   @IsOptional()
   level?: number;
 
+  @ApiProperty({ required: false })
   @IsEnum(Elo)
   @IsOptional()
   elo?: Elo
 
+  @ApiProperty()
   @IsInt()
   @IsNotEmpty()
   blueEssence: number;
 
+  @ApiProperty()
   @IsInt()
   @IsNotEmpty()
   skinQuantity: number;
 
+  @ApiProperty()
   @IsInt()
   @IsNotEmpty()
   championQuantity: number;
 
+  @ApiProperty({ type: Number })
   @IsDecimal()
   @IsNotEmpty()
   price: Prisma.Decimal;
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,15 +1,19 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNotEmpty, IsString, IsStrongPassword } from "class-validator";
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   username: string;
 
+  @ApiProperty()
   @IsString()
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty()
   @IsString()
   @IsStrongPassword()
   @IsNotEmpty()
