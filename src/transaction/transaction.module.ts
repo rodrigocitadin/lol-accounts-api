f075201db28dd2d1dc4c0c21c0f27c0ec6628bb0
citadin <citadinrodrigo@gmail.com>
2023-08-13 18:50:31 -0300

    add imports

--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TransactionService } from './transaction.service';
 import { TransactionController } from './transaction.controller';
+import { PrismaModule } from 'src/prisma.module';
+import { UserModule } from 'src/user/user.module';
+import { AccountModule } from 'src/account/account.module';
 
 @Module({
+  imports: [PrismaModule, UserModule, AccountModule],
   controllers: [TransactionController],
   providers: [TransactionService]
 })
-export class TransactionModule {}
+export class TransactionModule { }
