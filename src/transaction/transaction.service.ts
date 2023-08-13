45057c042975648bb2914d92c558e28f67ea8910
citadin <citadinrodrigo@gmail.com>
2023-08-13 18:50:52 -0300

    create transaction service

--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { CreateTransactionDto } from './dto/create-transaction.dto';
+import { AccountService } from 'src/account/account.service';
+import { UserService } from 'src/user/user.service';
+import { Decimal } from '@prisma/client/runtime/library';
 
 @Injectable()
-export class TransactionService {}
+export class TransactionService {
+  constructor(
+    private prisma: PrismaService,
+    private accountService: AccountService,
+    private userService: UserService,
+  ) { }
+
+  async create(id: string, createTransactionDto: CreateTransactionDto): Promise<void> {
+    const account = await this.accountService.findOne(createTransactionDto.accountId);
+    const { balance } = await this.userService.findOne(id);
+    const newBalance = Decimal.sub(balance, account.price);
+
+    if (newBalance.lessThanOrEqualTo(0)) {
+      throw new BadRequestException();
+    }
+
+    const transaction = await this.prisma.transaction.create({
+      data: {
+        payeeId: account.ownerId,
+        accountId: account.id,
+        ammount: account.price,
+      }
+    })
+
+    const userTransaction = await this.prisma.userTransaction.create({
+      data: {
+        userId: id,
+        transactionId: transaction.id,
+      }
+    })
+
+    if (!transaction || !userTransaction) {
+      throw new BadRequestException();
+    }
+
+    await this.userService.update(id, { balance: newBalance })
+    await this.accountService.update(account.id, { sold: true })
+  }
+}
