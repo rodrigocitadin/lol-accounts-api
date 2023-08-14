3171f8e2f580dfbe8ef22a38697f25725798b2ed
citadin <citadinrodrigo@gmail.com>
2023-08-14 14:22:43 -0300

    create transaction controller

--- src/transaction/transaction.controller.ts
+++ src/transaction/transaction.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Post, UseGuards } from '@nestjs/common';
 import { TransactionService } from './transaction.service';
+import { CreateTransactionDto } from './dto/create-transaction.dto';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
 
 @Controller('transaction')
 export class TransactionController {
-  constructor(private readonly transactionService: TransactionService) {}
+  constructor(private readonly transactionService: TransactionService) { }
+
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  @Post(':id')
+  async create(@Param('id') id: string, @Body() createTransactionDto: CreateTransactionDto) {
+    await this.transactionService.create(id, createTransactionDto);
+  }
 }
