970dba69c382842aaeef3f1eafa5c367364e1abb
citadin <citadinrodrigo@gmail.com>
2023-08-18 13:58:04 -0300

    add find by id

--- src/transaction/transaction.controller.ts
+++ src/transaction/transaction.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { TransactionService } from './transaction.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { ApiBearerAuth } from '@nestjs/swagger';
@@ -14,5 +14,11 @@ export class TransactionController {
   async create(@Body() createTransactionDto: CreateTransactionDto) {
     const soldAccount = await this.transactionService.create(createTransactionDto);
     return soldAccount;
+  }
+
+  @Get()
+  async findById(@Param('id') id: string) {
+    const transactions = await this.transactionService.findById(id);
+    return transactions;
   }
 }
