faa0c9b7f858faf729847ff8e0f48e7ce14862cd
rodrigocitadin <citadinrodrigo@gmail.com>
2023-09-03 00:27:02 -0300

    solving some bugs

--- src/user/dto/return-user.dto.ts
+++ src/user/dto/return-user.dto.ts
@@ -1,4 +1,8 @@
-import { OmitType } from "@nestjs/swagger";
-import { CreateUserDto } from "./create-user.dto";
+// import { OmitType } from "@nestjs/swagger";
+// import { CreateUserDto } from "./create-user.dto";
 
-export class ReturnUserDto extends OmitType(CreateUserDto, ['password'] as const) { }
+import { User } from "@prisma/client";
+
+// export class ReturnUserDto extends OmitType(CreateUserDto, ['password'] as const) { }
+
+export type ReturnUserDto = Pick<User, "id" | "email" | "balance" | "username">;
