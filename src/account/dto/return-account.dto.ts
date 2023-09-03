da95bc947e55edee145691cd6c927bf1ec12093a
rodrigocitadin <citadinrodrigo@gmail.com>
2023-09-03 00:30:23 -0300

    solving the same bugs but to account

--- src/account/dto/return-account.dto.ts
+++ src/account/dto/return-account.dto.ts
@@ -1,4 +1,8 @@
-import { OmitType } from "@nestjs/swagger";
-import { CreateAccountDto } from "./create-account.dto";
+// import { OmitType } from "@nestjs/swagger";
+// import { CreateAccountDto } from "./create-account.dto";
 
-export class ReturnAccountDto extends OmitType(CreateAccountDto, ['password'] as const) { }
+// export class ReturnAccountDto extends OmitType(CreateAccountDto, ['password'] as const) { }
+
+import { Account } from "@prisma/client";
+
+export type ReturnAccountDto = Omit<Account, "password" | "createdAt" | "updatedAt">
