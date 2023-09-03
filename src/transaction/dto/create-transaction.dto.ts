22c0147e7bf8d5ab087827ee336a988cc58e4c42
rodrigocitadin <citadinrodrigo@gmail.com>
2023-09-03 00:34:48 -0300

    change to string

--- src/transaction/dto/create-transaction.dto.ts
+++ src/transaction/dto/create-transaction.dto.ts
@@ -1,15 +1,14 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsUUID } from "class-validator";
-import { UUID } from "crypto";
 
 export class CreateTransactionDto {
   @ApiProperty()
   @IsUUID()
   @IsNotEmpty()
-  userId: UUID;
+  userId: string;
 
   @ApiProperty()
   @IsUUID()
   @IsNotEmpty()
-  accountId: UUID;
+  accountId: string;
 }
