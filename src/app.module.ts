3dba908d85b6484a198557806359075667153a4e
rodrigocitadin <citadinrodrigo@gmail.com>
2023-09-15 14:17:43 -0300

    last fine tune

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma.module';
 import { AccountModule } from './account/account.module';
@@ -10,7 +8,5 @@ import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), PrismaModule, AccountModule, UserModule, AuthModule, TransactionModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule { }
--- src/user/dto/return-user.dto.ts
+++ src/user/dto/return-user.dto.ts
@@ -1,8 +1,3 @@
-// import { OmitType } from "@nestjs/swagger";
-// import { CreateUserDto } from "./create-user.dto";
-
 import { User } from "@prisma/client";
-
-// export class ReturnUserDto extends OmitType(CreateUserDto, ['password'] as const) { }
 
 export type ReturnUserDto = Pick<User, "id" | "email" | "balance" | "username">;
