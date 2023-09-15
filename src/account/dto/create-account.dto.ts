695558209bb2247125284fefda12555bb6c4b57f
rodrigocitadin <citadinrodrigo@gmail.com>
2023-09-15 18:09:31 -0300

    removing useless types and api properties

--- src/account/dto/create-account.dto.ts
+++ src/account/dto/create-account.dto.ts
@@ -1,72 +1,33 @@
-import { IsBoolean, IsDecimal, IsEmail, IsEnum, IsInt, IsNotEmpty, IsOptional, IsString, IsStrongPassword } from "class-validator";
-import { Region, Elo } from '@prisma/client';
-import { ApiProperty } from "@nestjs/swagger";
+import { IsBoolean, IsDecimal, IsEmail, IsNotEmpty, IsOptional, IsString, IsStrongPassword } from "class-validator";
 import { Decimal } from "@prisma/client/runtime/library";
 
 export class CreateAccountDto {
-  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   username: string;
 
-  @ApiProperty()
   @IsString()
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
-  @ApiProperty({ required: false })
   @IsBoolean()
   @IsOptional()
   verifiedEmail: boolean;
 
-  @ApiProperty()
   @IsString()
   @IsStrongPassword()
   @IsNotEmpty()
   password: string;
 
-  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   ownerId: string;
 
-  @ApiProperty({ required: false, default: false })
   @IsBoolean()
   @IsOptional()
   sold?: boolean;
 
-  @ApiProperty()
-  @IsEnum(Region)
-  @IsNotEmpty()
-  region: Region;
-
-  @ApiProperty({ required: false })
-  @IsInt()
-  @IsOptional()
-  level?: number;
-
-  @ApiProperty({ required: false })
-  @IsEnum(Elo)
-  @IsOptional()
-  elo?: Elo
-
-  @ApiProperty()
-  @IsInt()
-  @IsNotEmpty()
-  blueEssence: number;
-
-  @ApiProperty()
-  @IsInt()
-  @IsNotEmpty()
-  skinQuantity: number;
-
-  @ApiProperty()
-  @IsInt()
-  @IsNotEmpty()
-  championQuantity: number;
-
-  @ApiProperty({ type: Number })
   @IsDecimal()
   @IsNotEmpty()
   price: Decimal;
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,26 +1,21 @@
-import { ApiProperty } from "@nestjs/swagger";
 import { Decimal } from "@prisma/client/runtime/library";
 import { IsDecimal, IsEmail, IsNotEmpty, IsOptional, IsString, IsStrongPassword } from "class-validator";
 
 export class CreateUserDto {
-  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   username: string;
 
-  @ApiProperty()
   @IsString()
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
-  @ApiProperty()
   @IsString()
   @IsStrongPassword()
   @IsNotEmpty()
   password: string;
 
-  @ApiProperty()
   @IsDecimal()
   @IsOptional()
   balance: Decimal
